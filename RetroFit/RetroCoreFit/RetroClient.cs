a3061eeb3eba40b70122e1186d601acd6d22c927
Akash Kava <ackava@gmail.com>
2019-06-19 13:52:21 +0530

    Added Single Create

--- RetroFit/RetroCoreFit/RetroClient.cs
+++ RetroFit/RetroCoreFit/RetroClient.cs
@@ -9,8 +9,15 @@ namespace RetroCoreFit
 
         public static T Create<T, TBase>(Uri baseUrl, HttpClient client = null)
             where T:class
+            where TBase: BaseService
         {
             return InterfaceBuilder.Instance.Build<T>(baseUrl, client, typeof(TBase));
+        }
+
+        public static T Create<T>(Uri baseUrl, HttpClient client = null)
+            where T:class
+        {
+            return InterfaceBuilder.Instance.Build<T>(baseUrl, client, typeof(BaseService));
         }
 
     }
