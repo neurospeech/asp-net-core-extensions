3c056c35e705f927416cd552ef47643c08c1775e
Akash Kava <39438041+ackava@users.noreply.github.com>
2019-10-07 09:17:56 +0530

    Added ApiResponse

---
+++ RetroFit/RetroCoreFit/ApiResponse.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+
+namespace RetroCoreFit
+{
+    public interface IApiResponse
+    {
+        void Initialize(HttpResponseMessage response, object model);
+
+        Type GetModelType();
+    }
+
+    public class ApiResponse<T> : IApiResponse
+    {
+        public Dictionary<string, string> Headers { get; protected set; }
+
+        public T Model { get; protected set; }
+
+        public virtual void Initialize(HttpResponseMessage response, object model)
+        {
+
+            this.Headers = new Dictionary<string, string>();
+
+            var headerProperties = this.GetType().GetProperties().Select(x => new
+            {
+                Property = x,
+                Header = x.GetCustomAttribute<HeaderAttribute>()
+            }).Where(x => x.Header != null)
+            .ToDictionary(x => x.Header.Name.ToLower(), x => x.Property);
+
+            foreach (var k in response.Headers)
+            {
+                this.Headers[k.Key] = k.Value.ToString();
+                if (headerProperties.TryGetValue(k.Key.ToLower(), out var p))
+                {
+                    object v = k.Value.ToString();
+                    if (p.PropertyType != typeof(string))
+                    {
+                        var t = Nullable.GetUnderlyingType(p.PropertyType) ?? p.PropertyType;
+                        v = Convert.ChangeType(v, t);
+                    }
+                    p.SetValue(this, v);
+                }
+            }
+
+            this.Model = (T)model;
+        }
+
+        Type IApiResponse.GetModelType()
+        {
+            return typeof(T);
+        }
+
+    }
+}
--- RetroFit/RetroCoreFit/BaseService.cs
+++ RetroFit/RetroCoreFit/BaseService.cs
@@ -55,49 +55,6 @@ namespace RetroCoreFit
         }
     }
 
-    public interface IApiResponse
-    {
-        void Initialize(HttpResponseMessage response, object model);
-
-        Type GetModelType();
-    }
-
-    public class ApiResponse<T>: IApiResponse
-    {
-        public Dictionary<string,string> Headers { get; protected set; }
-
-        public T Model { get; protected set; }
-
-        public virtual void Initialize(HttpResponseMessage response, object model)
-        {
-
-            this.Headers = new Dictionary<string, string>();
-
-            var headerProperties = this.GetType().GetProperties().Select(x => new {
-                Property = x,
-                Header = x.GetCustomAttribute<HeaderAttribute>()
-            }).Where(x => x.Header != null)
-            .ToDictionary(x => x.Header.Name.ToLower(), x=> x.Property);
-
-            foreach(var k in response.Headers)
-            {
-                this.Headers[k.Key] = k.Value.ToString();
-                if(headerProperties.TryGetValue(k.Key.ToLower(), out var p))
-                {
-                    p.SetValue(this, k.Value.ToString());
-                }
-            }
-
-            this.Model= (T)model;
-        }
-
-        Type IApiResponse.GetModelType()
-        {
-            return typeof(T);
-        }
-
-    }
-
     public class BaseService {
 
         public Uri BaseUrl { get; set; }
@@ -303,8 +260,7 @@ namespace RetroCoreFit
             if (response.IsSuccessStatusCode) {
 
 
-                Type gt = returnType.GetGenericTypeDefinition();
-                if (gt.GetInterfaces().Contains(typeof(IApiResponse)))
+                if (typeof(IApiResponse).IsAssignableFrom(returnType))
                 {
                     var rv = (object)Activator.CreateInstance<T>();
                     var irv = rv as IApiResponse;
