24a2b09ca660bc8ddd64ad5e17575507cc61a8d6
Akash Kava <ackava@gmail.com>
2021-04-19 13:22:31 +0530

    Added Separate AppCache and DI libraries

---
+++ NeuroSpeech.AppCache/AppCache.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Caching
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DIRegister(ServiceLifetime.Singleton)]
+    public class AppCache<T>
+    {
+        private IMemoryCache cache;
+        private string prefix;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cache"></param>
+        public AppCache(IMemoryCache cache)
+        {
+            this.cache = cache;
+            this.prefix = typeof(T).FullName + ":";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="factory"></param>
+        /// <returns></returns>
+        public T GetOrCreate(object key, Func<ICacheEntry, T> factory)
+        {
+            return cache.GetOrCreate(prefix + key, ci =>
+            {
+                // by default minimum expiration is one minute
+                ci.SetSlidingExpiration(TimeSpan.FromMinutes(1));
+                return factory(ci);
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="factory"></param>
+        /// <returns></returns>
+        public Task<T> GetOrCreateAsync(object key, Func<ICacheEntry, Task<T>> factory)
+        {
+            return AtomicGetOrCreateAsync(prefix + key, ci =>
+            {
+                // by default minimum expiration is one minute
+                ci.SetSlidingExpiration(TimeSpan.FromMinutes(1));
+                return factory(ci);
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(string key)
+        {
+            cache.Remove(prefix + key);
+        }
+
+        private object lockObject = new object();
+
+        private Task<T> AtomicGetOrCreateAsync(
+            string key,
+            Func<ICacheEntry, Task<T>> factory)
+        {
+            return cache.GetOrCreate<Task<T>>(key, (entry) =>
+            {
+                lock (lockObject)
+                {
+                    Func<ICacheEntry, Task<T>> fx = async (e2) => {
+                        try
+                        {
+                            return await factory(e2);
+                        }
+                        catch
+                        {
+                            // forcing getting out of lock...
+                            await Task.Delay(10);
+                            // it is stale... 
+                            lock (lockObject)
+                            {
+                                cache.Remove(key);
+                            }
+
+                            // awaiter must know that 
+                            // first request was unsuccessful 
+                            // and must try again...
+                            throw;
+                        }
+                    };
+                    return cache.GetOrCreate<Task<T>>(key, (e1) =>
+                    {
+                        return fx(e1);
+                    });
+                }
+            });
+        }
+    }
+}
---
+++ NeuroSpeech.DependencyInjectionExtensions/AssemblyRegistrationExtensions.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroSpeech
+{
+    [ExcludeFromCodeCoverage]
+    public static class AssemblyRegistrationExtensions
+    {
+
+        /// <summary>
+        /// Reads embedded string resource from given assembly
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="name"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static async Task<string> GetStringResourceAsync(
+            this Assembly assembly,
+            string name,
+            Encoding encoding = null)
+        {
+            using (var rs = assembly.GetManifestResourceStream(name))
+            {
+                using (var reader = new StreamReader(rs, encoding ?? Encoding.UTF8))
+                {
+                    return await reader.ReadToEndAsync();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reads embedded string resource from given assembly
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="name"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string GetStringResource(
+            this Assembly assembly,
+            string name,
+            Encoding encoding = null)
+        {
+            using (var rs = assembly.GetManifestResourceStream(name))
+            {
+                using (var reader = new StreamReader(rs, encoding ?? Encoding.UTF8))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Registers given assembly types for DI
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="assembly"></param>
+        public static void RegisterAssembly(this IServiceCollection services, Assembly assembly)
+        {
+
+            foreach (var type in assembly.GetExportedTypes())
+            {
+
+                try
+                {
+                    var a = type.GetCustomAttribute<DIRegisterAttribute>();
+                    if (a != null)
+                    {
+                        Type baseType = a.BaseType;
+
+                        Func<IServiceProvider, object> factory = null;
+                        if (a.Factory != null)
+                        {
+                            BaseDIFactory f = Activator.CreateInstance(a.Factory) as BaseDIFactory;
+                            factory = (sp) => f.CreateService(sp);
+                        }
+
+                        if (baseType != null)
+                        {
+                            services.Add(new ServiceDescriptor(baseType, type, a.Type));
+                        }
+                        else
+                        {
+                            if (factory != null)
+                            {
+                                services.Add(new ServiceDescriptor(type, factory, a.Type));
+                            }
+                            else
+                            {
+                                services.Add(new ServiceDescriptor(type,type, a.Type));
+                            }
+                        }
+                    }
+                }
+                catch { }
+            }
+
+        }
+
+    }
+}
