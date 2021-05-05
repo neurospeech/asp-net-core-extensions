9d0b0935e0520c1036eb02b6c49d85641a727469
Akash Kava <ackava@gmail.com>
2021-05-05 16:41:07 +0530

    StringExtensions moved

--- EFCoreLiveMigration/NeuroSpeech.EFCoreLiveMigration/StringExtensions.cs
+++ EFCoreLiveMigration/NeuroSpeech.EFCoreLiveMigration/StringExtensions.cs
@@ -11,22 +11,6 @@ namespace NeuroSpeech.EFCoreLiveMigration
     public static class StringExtensions
     {
 
-        public static bool ContainsIgnoreCase(this string text, string test) {
-            if (string.IsNullOrWhiteSpace(text))
-                return false;
-            if (string.IsNullOrWhiteSpace(test))
-                return false;
-            return text.IndexOf(test, StringComparison.OrdinalIgnoreCase) != -1;
-        }
-
-        public static bool EqualsIgnoreCase(this string text, string test) {
-            if (string.IsNullOrWhiteSpace(text))
-                return string.IsNullOrWhiteSpace(test);
-            if (string.IsNullOrWhiteSpace(test))
-                return false;
-            return text.Equals(test, StringComparison.OrdinalIgnoreCase);
-        }
-
         public static string GetColumnTypeForSql(this IProperty property) {
 
             string type = property.GetColumnType();
