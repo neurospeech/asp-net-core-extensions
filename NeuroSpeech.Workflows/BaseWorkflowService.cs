c6b6eac0d65927bdd8794516c4e1efff4d424e7a
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-05-22 11:30:00 +0530

    wip

--- NeuroSpeech.Workflows/BaseWorkflowService.cs
+++ NeuroSpeech.Workflows/BaseWorkflowService.cs
@@ -2,11 +2,19 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace NeuroSpeech.Workflows
 {
     public class BaseWorkflowService
     {
         internal protected TaskHubClient client;
+
+        public async Task RaiseEvent(string id, string name, object data = null)
+        {
+            var state = await client.GetOrchestrationStateAsync(id);
+            await client.RaiseEventAsync(state.OrchestrationInstance, name, data ?? "");
+        }
+
     }
 }
