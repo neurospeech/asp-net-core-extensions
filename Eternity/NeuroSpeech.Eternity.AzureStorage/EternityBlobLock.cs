1829592a697d2bb3804653cc74dc3cfcdea86eae
Akash Kava <ackava@gmail.com>
2021-06-23 11:53:19 +0530

    Eternity moved to new project

--- Eternity/NeuroSpeech.Eternity.AzureStorage/EternityBlobLock.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NeuroSpeech.Eternity
-{
-    public class EternityBlobLock : IEternityLock
-    {
-        public string LeaseID { get; set; }
-        public string LockName { get; set; }
-    }
-}
--- Eternity/NeuroSpeech.Eternity/EventResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NeuroSpeech.Eternity
-{
-    public class EventResult
-    {
-        public string? EventName { get; set; }
-        public string? Value { get; set; }
-    }
-}
